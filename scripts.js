dc4670ee658b23e051a62d6e7217054e76356c24
kathirmeyyappan <kathirmey@gmail.com>
2024-07-21 16:26:19 -0700

    adding overlay and refactor

--- scripts.js
+++ scripts.js
@@ -5,3 +5,18 @@ function hover(element) {
 function gone(element) {
     document.getElementById(element).src = "images/" + element + "-colored.png";
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const profileImage = document.getElementById('profileImage');
+    const overlay = document.querySelector('.overlay');
+
+    profileImage.addEventListener('mouseenter', () => {
+        profileImage.src = 'images/kathir.jpg'; // Change to your new image URL
+        overlay.style.opacity = '1'; // Show overlay
+    });
+
+    profileImage.addEventListener('mouseleave', () => {
+        profileImage.src = 'images/bebop.gif'; // Revert to original image URL
+        overlay.style.opacity = '0'; // Hide overlay
+    });
+});

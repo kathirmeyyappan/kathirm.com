5e702528cb007cdeee230ef72d4279289823c85b
kathirmeyyappan <kathirmey@gmail.com>
2024-07-22 08:58:22 -0700

    add click for profile and organize script

--- scripts.js
+++ scripts.js
@@ -1,68 +1,67 @@
-function hover(element) {
-    document.getElementById(element).src = "images/" + element + "-white.png";
-}
+const profileImage = document.getElementById('profileImage');
+const overlay = document.querySelector('.overlay');
+var hasClickedProfile = false;
 
-function gone(element) {
-    document.getElementById(element).src = "images/" + element + "-colored.png";
+let c = 0
+const pfps = [
+    'images/kathir.jpg', 
+    'images/bebop.gif', 
+    'images/luffy.gif',
+    'images/patrick.gif',
+    'images/shrek.gif', 
+    'images/subway-surfers.gif',
+    'images/error.gif'
+]
+
+function startFlashTimer() {
+    flashTimer = setTimeout(function() {
+        if (!hasClickedProfile) {
+            profileImage.classList.add("flash-border");
+        }
+    }, 500);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const profileImage = document.getElementById('profileImage');
-    const overlay = document.querySelector('.overlay');
-
-    let c = 0
-    const pfps = [
-        'images/kathir.jpg', 
-        'images/bebop.gif', 
-        'images/luffy.gif',
-        'images/patrick.gif',
-        'images/shrek.gif', 
-        'images/subway-surfers.gif',
-        'images/error.gif'
-    ]
-
-    profileImage.addEventListener('mouseenter', () => {
-        c = (c + 1) % pfps.length
-        profileImage.src = pfps[c]; // next pfp
-
-        overlay.style.opacity = '1'; // add overlay
-    });
-
+    
     // preload images
     pfps.forEach(src => {
         const img = new Image();
         img.src = src;
     });
 
+    var profileImage = document.getElementById("profileImage");
+    profileImage.style.cursor = 'pointer';
+
     profileImage.addEventListener('mouseleave', () => {
         overlay.style.opacity = '0'; // hide overlay
     });
-});
 
-document.addEventListener("DOMContentLoaded", function() {
-    var profileImage = document.getElementById("profileImage");
-    var hasHovered = false;
-    var flashTimer;
-
-    function startFlashTimer() {
-        flashTimer = setTimeout(function() {
-            if (!hasHovered) {
-                profileImage.classList.add("flash-border");
-            }
-        }, 500);
-    }
-
-    profileImage.addEventListener("mouseover", function() {
-        hasHovered = true;
+    profileImage.addEventListener("click", function() {
+        hasClickedProfile = true;
         clearTimeout(flashTimer);
         profileImage.classList.remove("flash-border");
+
+        c = (c + 1) % pfps.length
+        profileImage.src = pfps[c]; // next pfp
+        overlay.style.opacity = '1'; // add overlay
+
+        profileImage.style.cursor = 'default';
     });
 
     profileImage.addEventListener("mouseout", function() {
-        if (!hasHovered) {
+        profileImage.style.cursor = 'pointer';
+        if (!hasClickedProfile) {
             startFlashTimer();
         }
     });
 
     startFlashTimer();
 });
+
+function btn_hover(element) {
+    document.getElementById(element).src = "images/" + element + "-white.png";
+}
+
+function btn_gone(element) {
+    document.getElementById(element).src = "images/" + element + "-colored.png";
+}
